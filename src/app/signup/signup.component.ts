ff7b7492ca8344d47842825a2967413d6e1d7891
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-10-09 21:14:16 +0300

    finish form logic & validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
-  
-
+  submitForm(form: any) {
+    if (form.valid) {
+      console.log('Form submitted: ', form.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
